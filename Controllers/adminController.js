85cce5a3080e1cb965938a85e97ff71ae63fda8b
nguyen-huu-dung <72152089+nguyen-huu-dung@users.noreply.github.com>
2021-04-14 06:59:36 +0700

    update

--- Controllers/adminController.js
+++ Controllers/adminController.js
@@ -11,14 +11,14 @@ function getChoose(choosePage) {
 }
 
 
-//process musics admin
+//process musics admin - Vũ Hoàng Mai
 getChoose("musicsAdmin");
 
-//process upload admin
+//process upload admin - Vũ Hoàng Việt Dũng
 getChoose("uploadAdmin");
 
-//process request admin
+//process request admin - Dương Thành Đạt
 getChoose("requestAdmin");
 
-//process suggest admin
+//process suggest admin - Dương Thành Đạt
 getChoose("suggestionAdmin")
